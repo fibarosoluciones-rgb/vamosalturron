20810e78f527105f31b35408c46aaf8db8a66107
fibarosoluciones-rgb <fibarosoluciones@gmail.com>
2025-10-24 17:48:10 +0200

    Configure Firebase project settings

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,14 @@
 // Configura aquí tu proyecto de Firebase.
 // Sustituye los valores por los de tu panel de Firebase.
 window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
+  apiKey: "AIzaSyD12d4a_m7Hn1xQ1uBwogvqnaVsFdnJxXo",
+  authDomain: "vamosalturron-3242c.firebaseapp.com",
+  projectId: "vamosalturron-3242c",
+  storageBucket: "vamosalturron-3242c.firebasestorage.app",
+  messagingSenderId: "885711634201",
+  appId: "1:885711634201:web:565c8d958547964bc58f88",
+  measurementId: "G-NTWC6V4W4X"
 };
-
 // Puedes ajustar la colección/documento donde se guardará el estado compartido.
 window.FIREBASE_DATA_DOCUMENT = window.FIREBASE_DATA_DOCUMENT || {
   collection: 'app',
