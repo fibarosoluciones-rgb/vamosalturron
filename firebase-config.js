106ea1db8cbc9abf43afa62965e4f75bba9c0c8e
fibarosoluciones-rgb <fibarosoluciones@gmail.com>
2025-10-30 16:11:48 +0100

    Fix Firebase storage bucket domain

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@
         apiKey: "AIzaSyD12d4a_m7Hn1xQ1uBwogvqnaVsFdnJxXo",
         authDomain: "vamosalturron-3242c.firebaseapp.com",
         projectId: "vamosalturron-3242c",
-        storageBucket: "vamosalturron-3242c.firebasestorage.app",
+        storageBucket: "vamosalturron-3242c.appspot.com",
         messagingSenderId: "885711634201",
         appId: "1:885711634201:web:565c8d958547964bc58f88",
         measurementId: "G-NTWC6V4W4X"
